68a33dd8f46269fc2c990f19c32806a349c912cf
unknown <mtgmirage@protonmail.com>
2024-10-02 16:32:20 -0500

    a few changes

--- scripts/Submissions.js
+++ scripts/Submissions.js
@@ -2,13 +2,13 @@ export const SubmissionList = async () => {
     const response = await fetch("http://localhost:8088/submissions")
     const submissions = await response.json()
 
-    let submissionHTML = ""
+    let submissionsHTML = ""
     for (const submission of submissions) {
-        submissionHTML += `<section class="submission">
+        submissionsHTML += `<section class="submission">
             <div>Owns Jeans? ${submission.ownsBlueJeans}</div>
             <div>Area type foreign key? ${submission.socioLocationId}</div>
         </section>`
     }
 
-    return submissionHTML
+    return submissionsHTML
 }
